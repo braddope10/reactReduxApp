b93c1b3d9ba5897a389dc4038592b91fd2853bcd
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-04 22:30:46 -0700

    user scaffold

---
+++ api-lovesquare/rails-api/db/migrate/20201005053016_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.integer :age
+      t.string :city
+      t.string :state
+      t.string :sex
+      t.string :preference
+      t.string :profile_pic
+
+      t.timestamps
+    end
+  end
+end
