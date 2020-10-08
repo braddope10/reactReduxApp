094baad1e78497de7f5e74ad39ac83ccd35bacbb
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-08 16:40:10 -0700

    configured cors

--- api-lovesquare/rails-api/db/seeds.rb
+++ api-lovesquare/rails-api/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(bio: "", name: "Bradley", email: "braddope@hotmail.com", password_digest: "123456", city: "Las Vegas", state: "NV", sex: "Male", preference: "Straight", profile_pic: "", age: 21)
+
+Post.create(content: "wow", user_id: 5)
+
+Comment.create(text: "what", user_id: 2, post_id: 6)
