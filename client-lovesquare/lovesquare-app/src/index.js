503b46619ce7aa3bc404f2fc28d6b3013009eaa3
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-08 19:00:57 -0700

    imports

---
+++ client-lovesquare/lovesquare-app/src/index.js
@@ -0,0 +1,10 @@
+// react-redux, redux, thunk
+
+import React from 'react'
+import ReactDom from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
+// need to create a store for redux => createStore 1. reducer 2. dev tools 3.
