dea44bc9c9b1101f30805acfa62e74fe4f4c782c
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-08 19:44:48 -0700

    commentsReducer function

--- client-lovesquare/lovesquare-app/src/reducers/commentsReducer.js
+++ client-lovesquare/lovesquare-app/src/reducers/commentsReducer.js
@@ -1,6 +1,6 @@
-export const usersReducer = (state= [], action) => {
+export const commentsReducer = (state= [], action) => {
     switch(action.type){
-        case 'FETCH_USERS':
+        case 'FETCH_COMMENTS':
             return action.payload
         default:
             return state
