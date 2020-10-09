205d4a4cf89009c378c2f050783da40de93f09b3
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-08 20:09:08 -0700

    created users & posts container folders

---
+++ client-lovesquare/lovesquare-app/src/components/UsersContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UsersContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
