685852d8cfb9473bbdca7486fec036a9ff089abb
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-08 22:28:59 -0700

    User fetch redux action

---
+++ client-lovesquare/lovesquare-app/src/actions/usersActions.js
@@ -0,0 +1,7 @@
+export const fetchUsers = () => {
+    return (dispatch) => {
+        fetch(http://localhost:3000/users)
+        .then(resp => resp.json())
+        .then(users => console.log('fetchUsers', users))
+    }
+}
