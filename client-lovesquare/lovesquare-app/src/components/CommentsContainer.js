ab0f30824cc50b0dc181330b231da217cb625602
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-09 20:42:32 -0700

    finished CommentsContainer

--- client-lovesquare/lovesquare-app/src/components/CommentsContainer.js
+++ client-lovesquare/lovesquare-app/src/components/CommentsContainer.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchComments } from '../actions/commentsActions'
+import CommentsForm from './CommentsForm'
 
-export default class CommentsContainer extends Component {
+class CommentsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchComments()
+    }
+
     render() {
         return (
             <div>
-                
+                CommentsContainer
+                <CommentsForm />
             </div>
         )
     }
 }
+
+export default connect(null, { fetchComments })(CommentsContainer)
