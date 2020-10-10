3a1837aa4bb0a2e16de6b59153313e6014d87d80
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-09 17:53:38 -0700

    new imports

--- client-lovesquare/lovesquare-app/src/components/PostsContainer.js
+++ client-lovesquare/lovesquare-app/src/components/PostsContainer.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { postsReducer } from '../reducers/postsReducer'
+// import { postsReducer } from '../reducers/postsReducer'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions/postsActions'
+import PostsForm from './PostsForm'
 
 class PostsContainer extends Component {
     render() {
--- client-lovesquare/lovesquare-app/src/components/UsersContainer.js
+++ client-lovesquare/lovesquare-app/src/components/UsersContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
 import { fetchUsers } from '../actions/usersActions'
 import UsersForm from './UsersForm'
 
