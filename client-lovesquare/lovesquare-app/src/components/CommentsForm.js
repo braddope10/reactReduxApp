0266e59a6d397a0375c3407ccbf9e6c687218610
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-09 20:51:16 -0700

    finsihed CommetnsForm file

--- client-lovesquare/lovesquare-app/src/components/CommentsForm.js
+++ client-lovesquare/lovesquare-app/src/components/CommentsForm.js
@@ -1,11 +1,45 @@
 import React, { Component } from 'react'
 
-export default class CommentsForm extends Component {
+import { connect } from 'react-redux'
+
+import { addComment } from '../actions/commentsActions'
+
+class CommentsForm extends Component {
+
+    state = {
+        text: '',
+        user_id: this.user_id,
+        post_id: this.post_id
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target
+
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        this.props.addComment(this.state)
+    }
+
     render() {
         return (
-            <div>
-                
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <label>New Comment:</label>
+
+                <br />
+                <br />
+
+                <input type='text' value={this.state.text} onChange={this.handleChange} name="text" />
+
+                <br />
+                <br />
+
+                <input type='submit' value="Create Comment" />
+            </form>
         )
     }
 }
