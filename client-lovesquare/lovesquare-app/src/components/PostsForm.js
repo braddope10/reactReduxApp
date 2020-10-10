40e6dc8f8d4b9896dcd67fcebe434ce2100de446
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-09 18:06:17 -0700

    PostsForm state

--- client-lovesquare/lovesquare-app/src/components/PostsForm.js
+++ client-lovesquare/lovesquare-app/src/components/PostsForm.js
@@ -5,6 +5,20 @@ import { connect } from 'react-redux'
 import { addPosts } from '../actions/postsActions'
 
 class PostsForm extends Component {
+    
+    state = {
+        content: '',
+        user_id: ''
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     render() {
         return (
             <div>
