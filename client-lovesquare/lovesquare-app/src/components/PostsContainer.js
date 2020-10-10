b63443cb9e6bd90da245740d3098b928c7a9793a
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-09 17:55:48 -0700

    componentDidMount function was created

--- client-lovesquare/lovesquare-app/src/components/PostsContainer.js
+++ client-lovesquare/lovesquare-app/src/components/PostsContainer.js
@@ -5,13 +5,19 @@ import { fetchPosts } from '../actions/postsActions'
 import PostsForm from './PostsForm'
 
 class PostsContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts()
+    }
+
     render() {
         return (
             <div>
                 PostsContainer
+                <PostsForm />
             </div>
         )
     }
 }
 
-export default PostsContainer
+export default connect(null, { fetchPosts })(PostsContainer)
