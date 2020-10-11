9f32063128e6e1e86443ed02bc030f5cd4853b5a
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-10-10 18:48:31 -0700

    fixed User table

--- api-lovesquare/rails-api/db/migrate/20201005053016_create_users.rb
+++ api-lovesquare/rails-api/db/migrate/20201005053016_create_users.rb
@@ -1,16 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :bio
-      t.string :name
       t.string :email
       t.string :password_digest
-      t.string :city
-      t.string :state
-      t.string :sex
-      t.string :preference
-      t.string :profile_pic
-      t.integer :age
 
       t.timestamps
     end
