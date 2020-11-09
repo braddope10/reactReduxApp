73ddba18d1f18e5f642002a1616ee0ee555af478
Bradley Lopez <bradleylopezherrera@gmail.com>
2020-11-08 16:36:17 -0800

    fixed comments table

--- api-lovesquare/rails-api/db/migrate/20201007231512_create_comments.rb
+++ api-lovesquare/rails-api/db/migrate/20201007231512_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.string :associate
       t.string :text
-      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
